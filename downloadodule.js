0683e0674f1ef71629f18122ed0857cd967bb921
David <54854068+davidbuitech@users.noreply.github.com>
2021-08-05 09:17:43 -0500

    started notification module

--- downloadodule.js
+++ downloadodule.js
@@ -1,5 +1,5 @@
 var element = document.querySelector(
-  "#main > div > div._193wCc._3cVWns > div.shopee-top.shopee-top--sticky > div.navbar-wrapper.container-wrapper > div > div.flex.v-center._2oiwpp > a:nth-child(3) > div"
+  "#main > div > div._193wCc._3cVWns > div.shopee-top.shopee-top--sticky > div.navbar-wrapper.container-wrapper > div > ul > li > div"
 );
 
 element.addEventListener("mouseover", function () {
